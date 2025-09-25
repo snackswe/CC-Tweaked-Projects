862d4d93215d2f67fa76a1a2291dbb440f7af8cd
snackswe <snackswe@gmail.com>
2025-09-25 18:53:03 -0400

    Last Sync: 2025-09-25 18:53 (Mobile)

--- tableTest.lua
+++ tableTest.lua
@@ -1,49 +1,49 @@
 local arg1 = ...
-model = fs.open(arg1, "r")
-bP = textutils.unserialise(model.readAll())
-model.close()
-local special = {}
-foundNum = 1
+sensitive_test_file = fs.open(arg1, "r")
+loaded_blueprint = textutils.unserialise(sensitive_test_file.readAll())
+sensitive_test_file.close()
+local position_sensitive_blocks = {}
+total_sensitive_found = 1
 
-for x=1,#bP do
-    for y=1,#bP[x] do
-        for z=1,#bP[x][y] do
-            local cell = bP[x][y][z]
+for x=1,#loaded_blueprint do
+    for y=1,#loaded_blueprint[x] do
+        for z=1,#loaded_blueprint[x][y] do
+            local cell = loaded_blueprint[x][y][z]
             if cell.info.state and cell.info.state.facing then                    
-                if not special[foundNum] or special[foundNum].pos ~= cell.coords then
-                    table.insert(special, foundNum, {name = cell.info.name, facing = cell.info.state.facing, pos = cell.coords})
-                elseif special[foundNum].pos == cell.coords then
-                    table.insert(special, foundNum, {facing = cell.info.state.facing})
+                if not position_sensitive_blocks[total_sensitive_found] or position_sensitive_blocks[total_sensitive_found].pos ~= cell.coords then
+                    table.insert(position_sensitive_blocks, total_sensitive_found, {name = cell.info.name, facing = cell.info.state.facing, pos = cell.coords})
+                elseif position_sensitive_blocks[total_sensitive_found].pos == cell.coords then
+                    table.insert(position_sensitive_blocks, total_sensitive_found, {facing = cell.info.state.facing})
                 end
             end
             if cell.info.state and cell.info.state.half then
-                if not special[foundNum] or special[foundNum].pos ~= cell.coords then
-                    table.insert(special, foundNum, {name = cell.info.name, half = cell.info.state.half, pos = cell.coords})
-                elseif special[foundNum].pos == cell.coords then
-                    special[foundNum] = {name = cell.info.name, half = cell.info.state.half, facing = info.state.facing, pos = cell.coords}
+                if not position_sensitive_blocks[total_sensitive_found] or position_sensitive_blocks[total_sensitive_found].pos ~= cell.coords then
+                    table.insert(position_sensitive_blocks, total_sensitive_found, {name = cell.info.name, half = cell.info.state.half, pos = cell.coords})
+                elseif position_sensitive_blocks[total_sensitive_found].pos == cell.coords then
+                    position_sensitive_blocks[total_sensitive_found] = {name = cell.info.name, half = cell.info.state.half, facing = info.state.facing, pos = cell.coords}
                 end
             end
             if cell.info.state and cell.info.state.axis then
-                if not special[foundNum] or special[foundNum].pos ~= cell.coords then
-                    table.insert(special, foundNum, {name = cell.info.name, axis = cell.info.state.axis, pos = cell.coords})
-                elseif special[foundNum].pos == cell.coords then
-                    special[foundNum] = {name = cell.info.name, axis = cell.info.state.axis, half = info.state.half, pos = cell.coords}
+                if not position_sensitive_blocks[total_sensitive_found] or position_sensitive_blocks[total_sensitive_found].pos ~= cell.coords then
+                    table.insert(position_sensitive_blocks, total_sensitive_found, {name = cell.info.name, axis = cell.info.state.axis, pos = cell.coords})
+                elseif position_sensitive_blocks[total_sensitive_found].pos == cell.coords then
+                    position_sensitive_blocks[total_sensitive_found] = {name = cell.info.name, axis = cell.info.state.axis, half = info.state.half, pos = cell.coords}
                 end
             end
             if cell.info.state and (cell.info.state.axis or cell.info.state.half or cell.info.state.facing) then
-                foundNum = foundNum + 1
+                total_sensitive_found = total_sensitive_found + 1
             end
         end
     end
 end
 
-local model = fs.open("temp.data", "w")
-model.write(textutils.serialise(special, { compact = true }))
-model.close()
+local sensitive_test_file = fs.open("sensitive.data", "w")
+sensitive_test_file.write(textutils.serialise(position_sensitive_blocks, { compact = true }))
+sensitive_test_file.close()
 --[[
-for x=1,#bP do
-  for y=1,#bP[x] do
-    for z=1,#bP[x][y] do
+for x=1,#loaded_blueprint do
+  for y=1,#loaded_blueprint[x] do
+    for z=1,#loaded_blueprint[x][y] do
     end
   end
 end
